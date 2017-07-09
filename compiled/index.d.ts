57cf7285da4079ea1748ee05f7ecf8d91a323127
Łukasz Kużyński <wookieb>
2017-07-09 22:16:16 +0200

    Removed compiledversion, added missing sinon

--- compiled/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { ErrorsDomain, ErrorsDomainOptions, domainErrorDescriptor } from './ErrorsDomain';
-export { codeGenerator } from './codeGenerators';
-export declare const generators: {
-    incrementNumber: (startNumber?: number, step?: number) => () => string;
-    formatCode: (format: string, startNumber?: number, step?: number) => () => string;
-};
--- compiled/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const codeGenerators_1 = require("./codeGenerators");
-var ErrorsDomain_1 = require("./ErrorsDomain");
-exports.ErrorsDomain = ErrorsDomain_1.ErrorsDomain;
-exports.generators = {
-    incrementNumber: codeGenerators_1.incrementNumber,
-    formatCode: codeGenerators_1.formatCode
-};
